cbea49128b6c74173a72f6e25919dcbf9c760a5f
fabianofaustino13@gmail.com <q1w2e3r4>
2018-09-13 19:52:16 -0300

    Versao atualizada

--- DB_ASSEMBLEIA_CONSULTA2.sql
+++ DB_ASSEMBLEIA_CONSULTA2.sql
@@ -4,6 +4,9 @@ select pk_apa from tb_apartamentos join tb_blocos on pk_blo=fk_apa_blo where apa
 
 select count(*) from tb_apartamentos_moradores where fk_adm_apa='61' and fk_adm_mor='6';
 
+select count(*) from tb_apartamentos where pk_apa='1' and fk_apa_mor='2';
+
+
 insert into tb_apartamentos_moradores (fk_adm_apa, fk_adm_mor, ativo) values (61, 6, true);
 
 update tb_apartamentos_moradores set ativo=true where fk_adm_apa='61' and fk_adm_mor='6';
